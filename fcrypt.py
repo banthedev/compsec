a6adc5f42e5835765a9dfb29e3dbfad587828df8
Bryan Mointalvan <bryan_montalvanarevalo@student.uml.edu>
2023-10-18 14:37:04 -0400

    Initial commit

---
+++ fcrypt.py
@@ -0,0 +1,9 @@
+# Create simple CLI which is capable of encrypting and decrypting files using the Pretty Good Scheme
+# Group Members: James Pope, Bryan Montalvan
+# Technologies used: Pycryptodome, argprarse
+
+import Crypto
+import os
+
+
+
